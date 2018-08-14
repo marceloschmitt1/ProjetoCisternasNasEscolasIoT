1765b7ec5a8838665a4b3142f11e51a9bf311eb3
Marcelo Schmitt <marcelo.schmitt1@gmail.com>
2018-08-14 16:42:57 -0300

    Sketch Arduino envia timestampo correto
    Peguei emprestado duas funções do dj que fazem uma requisião NTP para obter o timestamp atual e transmití-lo junto como a medição de volume de água da cisterna. Refatorei um pouco o Sketch Arduino. Coloquei as biblicotecas utilizadas em uma pasta separada.

--- NodeMCU_cisterna_dev/NodeMCU_cisterna_dev.ino
+++ NodeMCU_cisterna_dev/NodeMCU_cisterna_dev.ino
@@ -1,200 +1,141 @@
 /*
   @author: Marcelo Schmitt
+
+  https://github.com/deusanyjunior
+  https://github.com/deusanyjunior/interscityroom8/blob/master/HTTPPostRequest.ino
 */
   
 #include <SPI.h>
 #include <ESP8266WiFi.h>
 
-#define DEBUG
-
-#ifdef DEBUG
-    #define D(X) X
-//    #define PUBNUB_DEBUG
-#else
-    #define D(X)
-#endif
-
-#define PubNub_BASE_CLIENT WiFiClient
-#include <PubNub.h>
-
-#define SERVO_PIN D6
-#define SHARP_PIN A0
+#include <ESP8266HTTPClient.h>
+#include <WiFiUdp.h>
+#include <time.h>
 
 #include <ArduinoJson.h>
 
-int status = WL_IDLE_STATUS;                // the Wifi radio's status
+#define DEBUG
+//#define VERBOSE
 
-// Wireless settings
+#ifdef DEBUG
+    #define D(X) X
+    #ifdef VERBOSE
+        #define PUBNUB_DEBUG
+        #define V(X) X
+    #else
+        #define V(X)
+    #endif
+#else
+    #define D(X)
+    #define V(X)
+#endif
+
+//////////////////Propriedades Wireless////////////////////////////
 const char* ssid = "MarDroid";
 const char* pass = "mirtero1";
+///////////////////////////////////////////////////////////////////
 
-// PubNub settings
-const static char pubkey[] = "pub-c-6ce39655-3209-4cea-acc9-543751fe4e55";         // your publish key 
-const static char subkey[] = "sub-c-6e52b6c2-8e6b-11e8-b7a4-ce74daf54d52";         // your subscribe key
-//const static char channel[] = "cisterna"; // channel to use
-const static char dados_channel[] = "dados_cisterna"; // channel to use
-const static char comando_channel[] = "comando_cisterna"; 
+////////////////Propriedades do PubNub/////////////////////////////
+#define PubNub_BASE_CLIENT WiFiClient
+#include <PubNub.h>
 
-// Constantes de calibracao e mensuracao
-#define INTERVALOS 6
-const float abscissas[] = {0.3, 0.35, 0.47, 0.57, 1.48, 1.96, 2.47};
-const float inclinacao[] = {-200, -166,67, -200, -65,93, -20,83, -19,60};
-const int ordenadas[] = {150, 140, 120, 100, 40, 30, 20};
+const static char pubkey[] = "pub-c-6ce39655-3209-4cea-acc9-543751fe4e55";
+const static char subkey[] = "sub-c-6e52b6c2-8e6b-11e8-b7a4-ce74daf54d52";
+const static char canal_de_dados[] = "dados_cisterna";
+const static char canal_de_comandos[] = "comando_cisterna";
+///////////////////////////////////////////////////////////////////
+
+/////////////////Recursos NTP//////////////////////////////////////
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "0.br.pool.ntp.org";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+// A time instance
+time_t rawTime;
+time_t startTime;
+time_t now;
+struct tm ts;
+char timeBuffer[80];
+bool timeDefined = false;
+///////////////////////////////////////////////////////////////////
+
+/////////////////Propriedades da celula de carga///////////////////
+//#define  ADDO  0    //Data Out //versao para ESP01
+//#define  ADSK  2    //SCK //versao para ESP01
+#define  ADDO  D0    //Data Out //versao para nodeMCU (ESP12)
+#define  ADSK  D2    //SCK //versao para nodeMCU (ESP12)
+
+//Este sktech utiliza uma versão adaptada da biblioteca HX711_ADC
+#include <HX711_ADC.h>
+
+//HX711 constructor (dout pin, sck pin)
+HX711_ADC LoadCell(D0, D2);
+
+#define fator_de_calibracao 696.0
+
+long ultima_leitura = millis();
+float forca = 0.0;
+///////////////////////////////////////////////////////////////////
+
+#define PINO_ATUADOR D6
 
 long inicio = millis();
 
 void setup() {
-  //pinMode(SHARP_PIN, INPUT);
-  pinMode(SERVO_PIN, OUTPUT);
-
-//  pwm.setup(SERVO_PIN, 100, 512);
+    Serial.begin(115200);
+    D(Serial.println("Iniciando celula de carga ...");)
+    LoadCell.begin();
+    long stabilisingtime = 5000; // tare preciscion can be improved by adding a few seconds of stabilising time
+    LoadCell.start(stabilisingtime);
+    LoadCell.setCalFactor(fator_de_calibracao); // user set calibration factor (float)
+    D(Serial.println("Startup + tare is complete");)
+    D(Serial.println();)
+    
+    pinMode(PINO_ATUADOR, OUTPUT);
   
-  Serial.begin(115200);
-  D(Serial.println("Serial set up");)
-
-  // attempt to connect using WPA2 encryption:
-  D(Serial.println("Attempting to connect to WPA network...");)
-
-  wifiConnection();
-
-  PubNub.begin(pubkey, subkey);
+//    pwm.setup(PINO_ATUADOR, 100, 512);
+    // attempt to connect using WPA2 encryption:
+    D(Serial.println("Attempting to connect to WPA network...");)
+    wifiConnection();
+    PubNub.begin(pubkey, subkey);
 }
-
-bool par = true;
 
 void loop() {
 
     if (WiFi.status() != WL_CONNECTED) {
         wifiConnection();
     } else {
-        //Leitura do sensor
-        int leitura_analogica = analogRead(SHARP_PIN);
-//        float voltagem_sensor = (1.0*analogRead(SHARP_PIN)/1024) * 5; //Conversão com Arduino
-        float voltagem_sensor = (1.0*leitura_analogica/1024) * 3.0;
+        //Get load cell reading
+        read_load_cell();
+
+        //TODO calculate real water measure
         
-        //Se a voltagem medida pelo sensor for menor que 0.4V ~ 130cm 
-        //então a leitura está errada pois da base até o topo da cisterna 
-        //há apenas 130cm de altura.
-        if (voltagem_sensor < 0.4) {
-            //leitura inconsistente
-            Serial.println("leitura maior que 130cm");
-            delay(1000);
-            return;
+        // Get current time from NTP
+        if (!timeDefined) {
+          if (!getTimeNTP()) return; // Get time from NTP servers
+          timeDefined = true;
         }
+        // set current time based on statup time
+        time (&rawTime);
+        now = rawTime + startTime;
+        ts = *localtime(&now);
+        strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%dT%H:%M:%S.000Z", &ts);
+
+        D(Serial.println(timeBuffer);)
+
+        String json_string = "{\"eon\":{\"time_stamp\":" + String(timeBuffer) + "," + "\"litros\":" + String(forca) + "}}";
         
-        //Se a voltagem medida pelo snsor for maior que 2.5V ~ 20cm 
-        //então a leitura está errada pois a água dentro da cisterna 
-        //deve chegar a no máximo 110cm de altura (no máximo a 20cm 
-        //de distância de sensor).
-        if (voltagem_sensor > 2.5) { //TODO fazer um #define calcular os filtros com base na distancia
-            //leitura inconsistente
-            Serial.println("leitura menor que 20cm");
-            delay(1000);
-            return;
-        }
-
-        D(Serial.print("leitura_analogica: ");)
-        D(Serial.print(leitura_analogica);)
-        D(Serial.print(" voltagem_sensor: ");)
-        D(Serial.print(voltagem_sensor);)
-        float distancia = calcula_distancia(voltagem_sensor);
-        D(Serial.print(" distancia: ");)
-        D(Serial.println(distancia);)
-
-        //Time stamp provisorio
-//        String str_dist = "[{\"tempo\":\"" + String((1.0*millis()-inicio)/1000) + "\"}," + "{\"distancia\":" + String(distancia) + "}]";
-//        String str_dist = "{\"eon\":[{\"tempo\":\"" + String((1.0*millis()-inicio)/1000) + "\"}," + "{\"distancia\":" + String(distancia) + "}]}";
-
-        String str_dist = "{\"eon\":{\"time_stamp\":" + String((1.0*millis()-inicio)/1000) + "," + "\"litros\":" + String(distancia) + "}}";
-
-        char msg[100];
-        str_dist.toCharArray(msg, 100);
-        Serial.println(msg);
+        publishToPubNub(json_string);
         
-        //Envio da mensagem para a nuvem (usando o servico do PubNub)
-        WiFiClient *client;        
-        client = PubNub.publish(dados_channel, msg);
-        if (!client) {
-            Serial.println("publishing error");
-            delay(1000);
-            return;
-        }
-        if (PubNub.get_last_http_status_code_class() != PubNub::http_scc_success) {
-            Serial.print("Got HTTP status code error from PubNub, class: ");
-            Serial.print(PubNub.get_last_http_status_code_class(), DEC);
-        }
-//        while (client->available()) {
-//            D(Serial.write(client->read());)
-//        }
-        client->stop();
-//        Serial.println("---");
-
-        Serial.println();
-        
-        // Parte de controle da cistenar (despejo de produto de limpeza)
-        String ordem = "";
-        
-//        Serial.println("retrieving message history");
-        client = PubNub.history(comando_channel, 1);
-        if (!client) {
-            Serial.println("history error");
-            delay(1000);
-            return;
-        }
-        while (client->connected()) {
-            while (client->connected() && !client->available()) ; // wait
-            char c = client->read();
-            ordem += c;
-//            Serial.print(c);
-        }
-//        Serial.print("ordem: ");
-//        Serial.print(ordem);
-        client->stop();
-//        Serial.println();
-
-        ordem.replace("[", "");
-        ordem.replace("]", "");
-//        Serial.print("ordem: ");
-//        Serial.print(ordem);
-//        Serial.println();
-
-        StaticJsonBuffer<200> jsonBuffer;
-        JsonObject& root = jsonBuffer.parseObject(ordem);
-        if (!root.success()) {
-          Serial.println("parseObject() failed");
-        }
-        const char* comando = root["comando"];
-//        Serial.print("comando: ");
-//        Serial.print(comando);
-//        Serial.println();
-
-        if (String(comando).equals("ativa")) {
-            analogWrite(SERVO_PIN, 800);
-        } else if (String(comando).equals("desativa")) {
-            analogWrite(SERVO_PIN, 100);
-        }
-        
+        checkPubNubHistory();
     }
     delay(1000);
   
-}
-
-float calcula_distancia(float leitura) {
-    int i;
-    for (i = 0; i < INTERVALOS+1; i++) 
-        if (leitura < abscissas[i]) break;
-
-//    D(Serial.print(ordenadas[i]);)
-//    D(Serial.print(" + ");)
-//    D(Serial.print(inclinacao[i]);)
-//    D(Serial.print("*(");)
-//    D(Serial.print(leitura);)
-//    D(Serial.print(" - ");)
-//    D(Serial.print(abscissas[i]);)
-//    D(Serial.print(")");)
-//    D(Serial.println();)
-    float distancia = ordenadas[i] + inclinacao[i]*(leitura - abscissas[i]);
-    return distancia;  
 }
 
 // Method to (re)connect to WiFi network
@@ -209,7 +150,163 @@ void wifiConnection() {
     }
     D(Serial.println("");)
     D(Serial.println("WiFi connected");)
-    D(Serial.print("IP address: ");)
+    D(Serial.print("Endereco IP: ");)
     D(Serial.println(WiFi.localIP());)
+}
+
+
+void read_load_cell() {
+    //update() should be called at least as often as HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS
+    //longer delay in scetch will reduce effective sample rate (be carefull with delay() in loop)
+    LoadCell.update();
+    //get smoothed value from data set + current calibration factor
+    if (millis() > ultima_leitura + 250) {
+        forca = LoadCell.getData();
+        D(Serial.print("Forca: ");)
+        D(Serial.print(forca);)
+        D(Serial.println();)
+        ultima_leitura = millis();
+    }
+    //receive from serial terminal
+      if (Serial.available() > 0) {
+          float i;
+          char inByte = Serial.read();
+          if (inByte == 't') LoadCell.tareNoDelay();
+      }
+    //check if last tare operation is complete
+    if (LoadCell.getTareStatus() == true) {
+        Serial.println("Tare complete");
+    }
+}
+
+void publishToPubNub(String json_string) {
+    V(Serial.println("PublishToPubNub");)
+    char json_char_array[100];
+    json_string.toCharArray(json_char_array, 100);
+    D(Serial.println(json_char_array);)
+    V(Serial.println("Publishing to PubNub ...");)
+    WiFiClient *client;
+    client = PubNub.publish(canal_de_dados, json_char_array);
+    if (!client) {
+        Serial.println("publishing error");
+        delay(1000);
+        return;
+    }
+    if (PubNub.get_last_http_status_code_class() != PubNub::http_scc_success) {
+        Serial.print("Got HTTP status code error from PubNub, class: ");
+        Serial.print(PubNub.get_last_http_status_code_class(), DEC);
+    }
+    V(Serial.println("Publich responce");)
+    V(while (client->available()) {)
+        V(Serial.write(client->read());)
+    V(})
+    client->stop();
+    free(client); // To avoid runnig out of memory
+}
+
+void checkPubNubHistory() {
+    V(Serial.println("Retrieving message history");)
+    WiFiClient *client;
+    client = PubNub.history(canal_de_comandos, 1);
+    if (!client) {
+        Serial.println("PubNub history error");
+        return;
+    }
+    String command_json = "";
+    while (client->connected()) {
+        while (client->connected() && !client->available()) ; // wait
+        char c = client->read();
+        command_json += c;
+    }
+    client->stop();
+    V(Serial.print("command_json: ");)
+    V(Serial.print(command_json);)
+    V(Serial.println();)
+    command_json.replace("[", "");
+    command_json.replace("]", "");
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject(command_json);
+    if (!root.success()) {
+      D(Serial.println("parseObject() failed");)
+    }
+    const char* command = root["comando"];
+    if (String(command).equals("ativa")) {
+        analogWrite(PINO_ATUADOR, 800);
+    } else if (String(command).equals("desativa")) {
+        analogWrite(PINO_ATUADOR, 100);
+    }
+    free(client); // To avoid runnig out of memory
+}
+
+// Method to get the NTP response and backup the *startTime*
+bool getTimeNTP() {
+    Serial.println("Get time from NTP servers");
+    //get a random server from the pool
+    WiFi.hostByName(ntpServerName, timeServerIP);
+  
+    sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+    // wait to see if a reply is available
+    delay(1000);
+  
+    int cb = udp.parsePacket();
+    if (!cb) {
+        Serial.println("no NTP packet yet");
+        delay(10000);
+        return false;
+    } else {
+        Serial.print("NTP packet received, length=");
+        Serial.println(cb);
+        // We've received a packet, read the data from it
+        udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+    
+        //the timestamp starts at byte 40 of the received packet and is four bytes,
+        // or two words, long. First, esxtract the two words:
+        unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+        unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    
+        // combine the four bytes (two words) into a long integer
+        // this is NTP time (seconds since Jan 1 1900):
+        unsigned long secsSince1900 = highWord << 16 | lowWord;
+    
+        // now convert NTP time into everyday time:
+        // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+        const unsigned long seventyYears = 2208988800UL;
+    
+        // subtract seventy years:
+        //unsigned long epoch = secsSince1900 - seventyYears;
+        now = secsSince1900 - seventyYears;
+    
+        // backup time after seventy years
+        time( &rawTime );
+        startTime = now - rawTime;
+    
+        Serial.println("Time defined");
+    
+        return true;
+    }
+}
+
+// Method to send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address) {
+    Serial.println("sending NTP packet...");
+    // set all bytes in the buffer to 0
+    memset(packetBuffer, 0, NTP_PACKET_SIZE);
+    // Initialize values needed to form NTP request
+    // (see URL above for details on the packets)
+    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+    packetBuffer[1] = 0;     // Stratum, or type of clock
+    packetBuffer[2] = 6;     // Polling Interval
+    packetBuffer[3] = 0xEC;  // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    packetBuffer[12]  = 49;
+    packetBuffer[13]  = 0x4E;
+    packetBuffer[14]  = 49;
+    packetBuffer[15]  = 52;
+  
+    // all NTP fields have been given values, now
+    // you can send a packet requesting a timestamp:
+    udp.beginPacket(address, 123); //NTP requests are to port 123
+    udp.write(packetBuffer, NTP_PACKET_SIZE);
+    udp.endPacket();
 }
 
